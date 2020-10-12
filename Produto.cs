762e4cfca9a8dfd0ce2016d3ffe32c714e407b5e
marcelochirai <65429412+marcelochirai@users.noreply.github.com>
2020-10-12 20:40:42 -0300

    Add files via upload

---
+++ Produto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace POO_TarefaConstrutores
+{
+    public class Produto
+    {
+
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        //Construtores - PERMITE OU OBRIGA O OBJETO RECEBER DADOS E DEPENDÊNCIAS NO MOMENTO DE SUA INSTANCIAÇÃO;
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        public void AdicionarProduto(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade = Quantidade - quantidade;
+        }
+        public override string ToString()
+        {
+            return Nome + ", R$ " + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + Quantidade
+            + " unidades, Total: R$ " + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace POO_TarefaConstrutores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Não é necessário instanciar o objeto no início
+            
+            Console.Write("Informe o nome do produto: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Qual o preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantidade no estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            //Instanciação do objeto p, sem sujeiras iniciais
+            Produto p = new Produto(nome, preco, quantidade);
+
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.Write("Digite o número de produtos a serem adicionados ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProduto(qte);
+
+            Console.WriteLine("Dados atualizados: " + p);
+
+            Console.Write("Digite o número de produtos a serem removidos do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+
+            Console.Write("Dados do atualizados: " + p);
+
+        }
+    }
+}
